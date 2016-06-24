ad6a11a83db04816339aa8ffc884844b28682181
jason_huang <jason_huang@gemteks.com>
2016-06-24 15:13:20 +0800

    [demo]0.0.3 Add update page

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,9 @@
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
 var UserModel = require('../models/user.js');
-var mqtt = require('mqtt');
+var LocalClient =  require('../models/localClient.js');
+var settings = require('../settings');
 
-var hostname = 'localhost';
-var portNumber = 1884;
-var mytopic= 'mqtt';
-var mUsers;
-
-var options = {
-	port:portNumber,
-    host: hostname,
-	protocolId: 'MQIsdp',
-	protocolVersion: 3
-};
-
-
-/* 1 */
-/*router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;*/
-
-/* 2 */
 module.exports = function(app) {
   app.get('/', function (req, res) {
 		res.render('index', { title: '首頁', 
@@ -31,9 +11,22 @@ module.exports = function(app) {
 		error: req.flash('error').toString()
 	});
   });
+
+  app.get('/update', function (req, res) {
+	UserModel.findOne({}, {}, { sort: { 'created_at' : -1 } }, function(err, user) {
+		console.log( "last record : "+user );
+		res.render('update', { title: '更新',
+			user: user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+	
+  });  
+
   app.get('/post', function (req, res) {
 	console.log('render to post.ejs');
-	res.render('post', { title: '推播',
+	res.render('post', { title: '上傳',
 		user: req.session.user,
 		success: req.flash('success').toString(),
 		error: req.flash('error').toString()
@@ -47,14 +40,9 @@ module.exports = function(app) {
 	var message = '{"name":"'+post_name+'","age":"'+post_age+'"}';
 	console.log('message:'+message);
 	
-	var client = mqtt.connect(options);
-
-	// publish 'Hello mqtt' to 'test'
-	client.publish(mytopic, message);
-
-	// terminate the client
-	client.end();
-	req.flash('success', '新增成功!');
+	LocalClient.publish(settings.mytopic, message);
+	LocalClient.end();
+	req.flash('success', '上傳完成!');
 	return res.redirect('/');
   });
 
@@ -63,37 +51,37 @@ module.exports = function(app) {
 	var find_name = req.flash('name').toString();
 	var find_age = req.flash('age').toString();
 	var successMessae,errorMessae;
-	console.log('查詢 name:'+find_name);
-	console.log('查詢 age:'+find_age);
+	console.log('name:'+find_name);
+	console.log('age:'+find_age);
 	
 	if(find_name.length>0){
 		console.log('find_name.length>0');
 		UserModel.find({ name: find_name }, function(err,users){
-			//查詢到的所有user
 			if(err){
-				console.log('查詢錯誤');  
+				console.log('find name:'+find_name);  
 				req.flash('error', err);
 				return res.redirect('/find');
 			}
-			console.log("查詢到user "+find_name+" : "+users);
+			console.log("find all of user "+find_name+" : "+users);
 			users.forEach(function(user) {
 				console.log('name:'+user.name + ', age :' +user.age);
 			});
 	  
 			if (users.length>0) {
-				console.log('查詢到'+users.length+'筆資料');
-				successMessae = '查詢到'+users.length+'筆資料';
-			}else{
-				console.log('查無資料');
-				errorMessae = '查無資料';
-				req.flash('error', err);
-      			return res.redirect('/find');
-	  		}
-	  		res.render('find', { title: '查詢',
+				console.log('find '+users.length+' records');
+				successMessae = '找到'+users.length+'筆資料';
+				res.render('find', { title: '查詢',
 					datas: users,
 					success: successMessae,
 					error: errorMessae
-	  		});
+				});
+			}else{
+				console.log('找不到資料!');
+				errorMessae = '找不到資料!';
+				req.flash('error', err);
+      			return res.redirect('/find');
+	  		}
+	  		
     	});
 	}else{
 		console.log('find_name.length=0');
